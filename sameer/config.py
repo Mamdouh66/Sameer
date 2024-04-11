a97db8f0a5d82c0b4c6aad3a1c98db78da9063a9
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-04-11 10:05:34 +0300

    🔧 ADD: Config file for env vars

---
+++ sameer/config.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    OMDP_API_KEY = os.getenv("OMDP_API_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_URL = os.getenv("QDRANT_URL")
+
+
+settings = Settings()
